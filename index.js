50b4531a67b38c92a6927723661453c3ee384429
Evgeniya Polyakova <evgeniyapolyakova050000@yandex.ru>
2021-07-12 17:51:24 +0300

    Fixed errors

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const cors = require('cors');
-
+const fetch=require('node-fetch')
 const server = express();
 server.use(express.json());
 
-server.listen(3001, () => {
-  console.log("server server");
-});
+
 
 server.use(cors());
 
-server.get("/", (req, res) => {
-    const { body } = req;
-    fetch(req.body).then(res=>res.send(res))
+server.post("/", (req, response) => {
+    console.log(req.body)
+    fetch(req.body.url).then(res => res.text()).then((data) => console.log(data))
+      
+    
+    .then(res=> response.send(res))
+  });
+
+  server.listen(3001, () => {
+    console.log("server server");
   });
